6b39577c21f13a9309df4eee8cb46fd7fb3404f4
quanghuy1258 <quanghuy1258@gmail.com>
2017-07-14 08:58:01 +0700

    Update win.c

--- win.c
+++ win.c
@@ -25,9 +25,9 @@ void OnDataReceived(void *param) {
 		if (iResult > 0)
 			printf("\n[FRIEND] %s\n", recvbuf);
 		else if (iResult == 0)
-			printf("Connection closed\n");
+			printf("Connection closed: socket[ %u ]\n", soc);
 		else
-			printf("recv failed with error: %d\n", WSAGetLastError());
+			printf("recv failed with error: %d socket[ %u ]\n", WSAGetLastError(), soc);
 	} while (iResult > 0);
 	_endthread();
 }
@@ -44,6 +44,7 @@ int clientMode(char *ipServer, char *port) {
 		printf("WSAStartup failed with error: %d\n", iResult);
 		return 1;
 	}
+	else printf("WSAStartup: success\n");
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
@@ -54,6 +55,7 @@ int clientMode(char *ipServer, char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("getaddrinfo: success\n");
 	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
 		if (ConnectSocket == INVALID_SOCKET) {
@@ -61,12 +63,15 @@ int clientMode(char *ipServer, char *port) {
 			WSACleanup();
 			return 1;
 		}
+		printf("? - connect: ai_flags [ %d ] ai_family[ %d ] ai_socktype[ %d ] ai_protocol[ %d ] sin_family[ %hu ] sin_addr[ %lu ] sin_port[ %hu ]\n",
+			ptr->ai_flags, ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol, ((SOCKADDR_IN*)ptr->ai_addr)->sin_family, ((SOCKADDR_IN*)ptr->ai_addr)->sin_addr.s_addr, ((SOCKADDR_IN*)ptr->ai_addr)->sin_port);
 		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
 		if (iResult == SOCKET_ERROR) {
 			closesocket(ConnectSocket);
 			ConnectSocket = INVALID_SOCKET;
 			continue;
 		}
+		else printf("Connect: socket[ %u ]\n", ConnectSocket);
 		break;
 	}
 	freeaddrinfo(result);
@@ -102,7 +107,7 @@ int clientMode(char *ipServer, char *port) {
 int serverMode(char *port) {
 	WSADATA wsaData;
 	int iResult;
-	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	struct addrinfo *result = NULL, hints;
 	SOCKET ListenSocket = INVALID_SOCKET;
 	SOCKET ClientSocket = INVALID_SOCKET;
 	char sendbuf[DEFAULT_BUFLEN] = { 0 };
@@ -112,6 +117,7 @@ int serverMode(char *port) {
 		printf("WSAStartup failed with error: %d\n", iResult);
 		return 1;
 	}
+	else printf("WSAStartup: success\n");
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
@@ -123,6 +129,7 @@ int serverMode(char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("getaddrinfo: success\n");
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	if (ListenSocket == INVALID_SOCKET) {
 		printf("socket failed with error: %ld\n", WSAGetLastError());
@@ -130,6 +137,7 @@ int serverMode(char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("socket: success [ %u ]\n", ListenSocket);
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
 		printf("bind failed with error: %d\n", WSAGetLastError());
@@ -138,6 +146,8 @@ int serverMode(char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("bind: ai_flags [ %d ] ai_family[ %d ] ai_socktype[ %d ] ai_protocol[ %d ] sin_family[ %hu ] sin_addr[ %lu ] sin_port[ %hu ]\n",
+		result->ai_flags, result->ai_family, result->ai_socktype, result->ai_protocol, ((SOCKADDR_IN*)result->ai_addr)->sin_family, ((SOCKADDR_IN*)result->ai_addr)->sin_addr.s_addr, ((SOCKADDR_IN*)result->ai_addr)->sin_port);
 	freeaddrinfo(result);
 	iResult = listen(ListenSocket, 3);
 	if (iResult == SOCKET_ERROR) {
@@ -146,6 +156,7 @@ int serverMode(char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("listen: success\n");
 	ClientSocket = accept(ListenSocket, NULL, NULL);
 	if (ClientSocket == INVALID_SOCKET) {
 		printf("accept failed with error: %d\n", WSAGetLastError());
@@ -153,6 +164,7 @@ int serverMode(char *port) {
 		WSACleanup();
 		return 1;
 	}
+	else printf("accept: socket[ %u ]\n", ClientSocket);
 	closesocket(ListenSocket);
 	handle = (HANDLE)_beginthread(OnDataReceived, 0, &ClientSocket);
 	printf("Chat started\n");
